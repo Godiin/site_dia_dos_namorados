4c6bde8ae90e9e21032ae7e0c9f1608f8efa1ff0
Godiin <ygorsilva305@gmail.com>
2025-06-12 16:14:59 -0300

    Primeiro commit do site de Dia dos Namorados

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+// Nenhuma interação do usuário por enquanto, apenas estrutura básica.
+
+// Futuramente, você pode adicionar funções aqui como controle de carrossel manual.
+// Exemplo:
+// const track = document.querySelector('.carousel-track');
+// let index = 0;
+// function moveCarousel() {
+//   index = (index + 1) % 3;
+//   track.style.transform = `translateX(-${index * 100}%)`;
+// }
+// setInterval(moveCarousel, 4000);
